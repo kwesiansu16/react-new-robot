1820aafb716a30ed3d7e66c8eb0fcdea9e84cdbf
Kwesi Ansu-Kyeremeh <kwesiansu1@gmail.com>
2023-06-26 13:31:12 -0700

    set name

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
 import { Component } from 'react';
+import logo from './logo.svg';
 
-class App extends Component() {
-  constructor() {
+import './App.css';
+
+
+class App extends Component {
+  constructor(){
     super();
+    this.state = {
+      name:'Sandy'
+    }
   }
-render(){
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-       Hello My name is Sandy
-        </p>
-    <button>Change Name</button>
-      </header>
-    </div>
-  );
-}
  
-}
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+         Hello My name is {this.state.name}
+          </p>
+      <button onClick={()=>{
+        this.setState({name:'Kofi'})
+      }}>Change Name</button>
+        </header>
+      </div>
+    );
+  }
+    
+  }
+  
+  
+  
+ 
+
 
 export default App;
